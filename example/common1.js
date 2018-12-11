18c53ba2217fa1b736876ae54ce6ace2f62fde88
“zhoufangbing” <1978467950@qq.com>
2018-12-11 12:19:17 +0800

    demo

--- example/common1.js
+++ example/common1.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const data = require('./data'); // 引入数据模块
+const data = require('../data'); // 引入数据模块
 
 const app = http.createServer( (req,res) => {
   res.writeHead(200, {"Content-Type":"text/html:charset='utf-8'"});
